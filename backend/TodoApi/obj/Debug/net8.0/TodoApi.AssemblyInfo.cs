5675e17899fc93c96d6c30a1c85caeadd15ee4cc
jfva01 <jvargasascanio@hotmail.com>
2026-03-23 11:56:25 -0300

    feat: update assembly info, project paths, and add new filter images

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ecd5a951c1dcea52597e501e79c91cce182943")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577e7a7d2fc3fb2ae4bb0de1863ad712ec6bff9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
