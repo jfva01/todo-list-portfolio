ba8ce84584de7b8acc201f7589617530e3fd79f2
jfva01 <jvargasascanio@hotmail.com>
2026-04-02 20:35:28 -0300

    Features(security): Reforzamiento de la propiedad mediante Id de validaciones para update, delete y get.

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a2037a43abe8b94a78a647339109bb9af17f96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9bcd1e31769dcdd3e90d55bc2a6351c20bb63b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
