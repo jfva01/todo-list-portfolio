116bbdd0b5cdaf0aedb178fc35d5f1f3012ebe93
jfva01 <jvargasascanio@hotmail.com>
2026-04-13 12:52:38 -0400

    feature: add backend deploy pipeline

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28de7852db57b0a4ca6d8c786d30b29daa2ad223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ad81f53d19ed12ec1939dfc41dc38035641ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
